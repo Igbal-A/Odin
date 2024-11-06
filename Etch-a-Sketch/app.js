9932d775a2c31e620b4a4ada6b3b60e02f166b2e
Igbal <igbal.alyasov@mail.ru>
2024-11-06 20:09:01 +0300

    Создал Таблицу и функционал остатка следа на таблице

--- Etch-a-Sketch/app.js
+++ Etch-a-Sketch/app.js
@@ -1,24 +1,37 @@
 let container = document.querySelector(".container__map");
-let blockDiv = document.querySelector(".map");
 
-function createMap () {
+function createGrid () {
     for (let x = 0; x < 16; x++) {
         for (let y = 0; y < 16; y++) {
             let block = document.createElement('div');
-            block.setAttribute("class", "map");
+            block.setAttribute("class", "grid");
             container.append(block);
         }
     }
 }
 
-createMap();
+createGrid();
 
-container.onmouseover = function (event) {
+container.addEventListener("mouseover", function (event) {
     let target = event.target;
-    target.style.background = "red";
-};  
+    let checkColor = target.style.background;
 
-container.onmouseout = function(event) {
+    if ((checkColor == "null" || checkColor == "") && target.className == "grid") {
+        target.style.background = "yellow";
+        target.style.opacity = 0.2;  
+        return 
+    }
+
+    if (event.target.style.background == "yellow") {
+        let op = Number(event.target.style.opacity);
+        op += 0.2;
+        target.style.opacity = op;
+    } 
+
+    console.log(event);
+});
+
+/* container.onmouseout = function(event) {
     let target = event.target;
     target.style.background = 'aqua';
-}
+} */
