fbb182eb24eb233a82e947b0f92759d2301914a0
Igbal <igbal.alyasov@mail.ru>
2024-11-08 12:54:43 +0300

    При создании новой сетки исправил ошибку при сохранении цвета для новой сетки

--- Etch-a-Sketch/app.js
+++ Etch-a-Sketch/app.js
@@ -1,73 +1,19 @@
-/* let container = document.querySelector(".container__map");
-let button = document.querySelector("#btn");
-let inputColorValue = document.querySelector("#color").value; */
-
-
-/* function createGrid () {
-    for (let x = 0; x < 16; x++) {
-        for (let y = 0; y < 16; y++) {
-            let block = document.createElement('div');
-            block.setAttribute("class", "grid");
-            container.append(block);
-        }
-    }
-} */
-
-
-/* container.addEventListener("mouseover", function (event) {
-    let target = event.target;
-    let checkColor = target.style.background;
-
-    if ((checkColor == "null" || checkColor == "") && target.className == "grid") {
-        target.style.background = inputColor;
-        colorGrid = target.style.background
-        target.style.opacity = 0.2;  
-        return 
-    }
-
-    if (event.target.style.background == colorGrid) {
-        let op = Number(event.target.style.opacity);
-        op += 0.2;
-        target.style.opacity = op;
-    } 
-
-    console.log(event);
-}); */
-
-/* function changeColor (color) {
-    let catchColorGrid;
-    container.addEventListener("mouseover", function (event) {
-        let target = event.target;
-        let checkColor = target.style.background;
-    
-        if ((checkColor == "null" || checkColor == "") && target.className == "grid") {
-            console.log(inputColorValue)
-            target.style.background = inputColorValue;
-            catchColorGrid = target.style.background
-            target.style.opacity = 0.2;  
-            return 
-        }
-    
-        if (event.target.style.background == catchColorGrid) {
-            let op = Number(event.target.style.opacity);
-            op += 0.2;
-            target.style.opacity = op;
-        } 
-    
-        console.log(event);
-    });
-} */
 
 
 
 function startToDraw () {  
+    let haveGrid = false;
     document.querySelector("#btn").addEventListener("click", function () {
         let container = document.querySelector(".container__map");
         let rows = document.querySelector("#rows").value;
         let colomns = document.querySelector("#colomns").value;
-        let inputColorValue = document.querySelector("#color").value;
+        /* let inputColorValue = document.querySelector("#color").value; */
 
-            function createGrid (rows, colomns) {
+            function createGrid (rows, colomns, havegrid) {
+                if (havegrid) {
+                    removeGrid();
+                }
+
                 if (rows < 100 && colomns < 100) {
                     for (let x = 0; x < rows; x++) {
                         for (let y = 0; y < colomns; y++) {
@@ -76,30 +22,29 @@ function startToDraw () {
                             container.append(block);
                         }
                     }
+                    haveGrid = true;
                     return
                 }
-                return alert("mistake, more less input colomns and rows ")
+                return alert("Введите число строки и столбцов меньше 100")
             }
 
             function removeGrid () {
-                btnRemove = document.querySelector("#btn__remove");
-                btnRemove.addEventListener("click", function () {
-                    mapDiv = document.querySelectorAll(".grid");
-                    mapDiv.forEach(element => {
-                        element.remove()
-                    });
-                })
+                mapDiv = document.querySelectorAll(".grid");
+                mapDiv.forEach(element => {
+                    element.remove()
+                });
             }
 
-            function changeColor (color) {
+            function changeColor () {
                 let catchColorGrid;
                 container.addEventListener("mouseover", function (event) {
+                    let inputColorValue = document.querySelector("#color").value;
                     let target = event.target;
                     let checkColor = target.style.background;
                 
                     if ((checkColor == "null" || checkColor == "") && target.className == "grid") {
-                        target.style.background = color;
-                        catchColorGrid = target.style.background
+                        target.style.background = inputColorValue;
+                        catchColorGrid = target.style.background;
                         target.style.opacity = 0.2;  
                         return 
                     }
@@ -113,14 +58,14 @@ function startToDraw () {
                 });
             }
 
-        changeColor(inputColorValue);
-        createGrid(rows, colomns);
+            function changeSize () {
+
+            }
+
+            createGrid(rows, colomns, haveGrid);
+            changeColor();
     });
 }
 
 startToDraw();
 
-/* container.onmouseout = function(event) {
-    let target = event.target;
-    target.style.background = 'aqua';
-} */
